a4b1328e5e90be050d457bb869d2b3884724035a
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2014-03-01 14:01:23 +0100

    Models done

--- models.py
+++ models.py
@@ -1,54 +1,64 @@
+from app import db
+
 
 class User(db.Model):
+    """User which owns resources behind our own api"""
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True)
     password = db.Column(db.String(40))
-    _scopes = db.Column(db.Text)
-
-    @property
-    def scopes(self):
-        if self._scopes:
-            return self._scopes.split()
-        return []
 
 
 class Client(db.Model):
+    """http://tools.ietf.org/html/rfc6749#section-2
 
-    client_id = db.Column(db.String(40), primary_key=True)
-    client_type_ = db.Column(db.Enum(('public', 'confidentail')))
-    _allow_grant_types = db.Column( )
+    RFC 6749 Section 2 describes clients.
 
-    @property
-    def client_secret():
-        return None
+    One thing to note is that redirection URIs are mandatory for clients. We
+    skip this requrement as this example will only allow the resource owner
+    password credentials grant(described in section 4.3).
 
-    @property
-    def default_redirect_uri():
-        return None
+     +----------+
+     | Resource |
+     |  Owner   |
+     |          |
+     +----------+
+          v
+          |    Resource Owner
+         (A) Password Credentials
+          |
+          v
+     +---------+                                  +---------------+
+     |         |>--(B)---- Resource Owner ------->|               |
+     |         |         Password Credentials     | Authorization |
+     | Client  |                                  |     Server    |
+     |         |<--(C)---- Access Token ---------<|               |
+     |         |    (w/ Optional Refresh Token)   |               |
+     +---------+                                  +---------------+
 
-    @property
-    def default_scopes():
-        return []
+    In this flow the Authorization Server will not redirect the user as
+    described in subsection 3.1.2 (Redirection Endpoint).
 
-    @property
-    def allowed_grant_types():
-        if _allow_grant_types:
-            return _allow_grant_types.split()
-        return []
+    """
 
-    def validate_scopes(self, client_id, scopes, client, request,
-                        *args, **kwargs):
-        return True
+    id = db.Column(db.String(40), primary_key=True)
+    type = db.Column(db.String(40))
 
 
 class Token(db.Model):
+    """Token that allows access to the API
+
+    Note that an access token is aware about to which user and client it was
+    issued, thus we can identify which user is making a request based on this
+    token.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
 
     client_id = db.Column(
         db.String(40), db.ForeignKey('client.client_id'),
         nullable=False,
     )
-
     client = db.relationship('Client')
 
     user_id = db.Column(
@@ -62,10 +72,3 @@ class Token(db.Model):
     access_token = db.Column(db.String(255), unique=True)
     refresh_token = db.Column(db.String(255), unique=True)
     expires = db.Column(db.DateTime)
-    _scopes = db.Column(db.Text)
-
-    @property
-    def scopes(self):
-        if self._scopes:
-            return self._scopes.split()
-        return []
